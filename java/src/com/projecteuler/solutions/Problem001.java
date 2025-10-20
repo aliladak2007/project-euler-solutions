b0d0e128433da53a5f8f1bff21eee477f136b12e
AMLadak07 <ali.ladak2007@gmail.com>
2025-10-20 12:03:35 +0100

    Solved my first problem

---
+++ java/src/com/projecteuler/solutions/Problem001.java
@@ -0,0 +1,23 @@
+package com.projecteuler.solutions;
+
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+public class Problem001 {
+    public static int sumOfMultiples(int limit) {
+        int sum = 0;
+        for (int i = 1; i < limit; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int result = sumOfMultiples(1000);
+        System.out.println("Sum of multiples of 3 or 5 below 1000: " + result);
+    }
+}
